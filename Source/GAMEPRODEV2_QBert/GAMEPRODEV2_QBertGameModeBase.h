89798bd12c8b94b479493bab54a307fc6d7febf9
Edisson Diolanda Flores <flores96@gmail.com>
2017-01-27 15:01:00 -0500

    GPD2 - QBert v0.0.1.0
    - Initial commit

---
+++ Source/GAMEPRODEV2_QBert/GAMEPRODEV2_QBertGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "GAMEPRODEV2_QBertGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEPRODEV2_QBERT_API AGAMEPRODEV2_QBertGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
