3fdb4cf92aef1e60de5816e883cfdf3398a3cb36
SOD-SJH-520-08W\graphic <flores96@gmail.com>
2017-02-10 14:44:23 -0500

    GPD2 - QBert v0.0.1.0 (again) Unreal 4.14.3
    - First commit
    
    GroundBlock.h/cpp
    - Self-explanitory
    - BUG: Unreal crashes when you try to make Blueprint out of C++

---
+++ Source/GAMEPRODEV2_QBert/GroundBlock.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GAMEPRODEV2_QBert.h"
+#include "GroundBlock.h"
+
+
+// Sets default values
+AGroundBlock::AGroundBlock()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	m_BlockRoot = CreateDefaultSubobject<USceneComponent>(TEXT("BlockRoot"));
+	RootComponent = m_BlockRoot;
+
+	m_BlockMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BlockMesh"));
+	m_BlockMesh->AttachToComponent(m_BlockRoot, FAttachmentTransformRules::SnapToTargetIncludingScale);
+
+	m_BlockCollider = CreateDefaultSubobject<UShapeComponent>(TEXT("BlockBox"));
+	m_BlockCollider->SetWorldScale3D(FVector(1.0f, 1.0f, 1.0f));
+
+}
+
+// Called when the game starts or when spawned
+void AGroundBlock::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGroundBlock::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
