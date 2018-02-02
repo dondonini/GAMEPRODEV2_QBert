050bb5e4d2d4ad67cf54b0287d7235645d405650
Edisson Flores <flores96@gmail.com>
2018-02-02 09:52:23 -0500

    Full Reset & Switching to Unity
    I hate Unreal because I don't know how to use it. I'll learn some day...
    
    Version: 0.0.0.0
    Unity Version: 2017.1.0f3
    
    INITIAL COMMIT

--- Source/GAMEPRODEV2_QBert/GAMEPRODEV2_QBert.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "GAMEPRODEV2_QBert.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAMEPRODEV2_QBert, "GAMEPRODEV2_QBert" );
--- Source/GAMEPRODEV2_QBert/GameplayerController.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "GAMEPRODEV2_QBert.h"
-#include "GameplayerController.h"
-
-
-
-
