94a0e11dd60e79e904504971c34164ee6fd895ae
Edisson Flores <flores96@gmail.com>
2018-04-25 02:19:25 -0400

    Attempting to rebuild the AI system using State Machines
    Version: 0.2.5.0
    Unity Version: 2017.3.1f1
    
    GameScene.unity
    - Disabled all AI scripts on all enemies
    
    NEW - GreenBlob_SM.cs
    - Desc: The state machine for the Green Blob character
    - Inital coding
    - TODO: Create all states (Line 21)
    
    NEW - GB_FollowState.cs
    - Desc: Follow state for the Green Blob
    - Initial coding
    
    NEW - EnemyStates_SM.cs
    - Desc: An interface holding all states for enemies

---
+++ Assets/Scripts/AIScripts/EnemyStates_SM.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface EnemyStates_SM {
+
+    // Updates the state
+    void UpdateState();
+
+    // State when entering map
+    void ToEnterState();
+
+    // State when following player
+    void ToFollowState();
+
+    // State when leaving map
+    void ToExitState();
+}
---
+++ Assets/Scripts/AIScripts/Enemy_GreenBlob/GreenBlob_SM.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenBlob_SM : MonoBehaviour {
+
+    [Tooltip("The speed the AI is going to chase the player in seconds.")]
+    public float m_chaseSpeed = 1.0f;
+
+    [HideInInspector]
+    public Transform m_chaseTarget = null;
+
+    // //////
+    // States
+    // //////
+    [HideInInspector]
+    public EnemyStates_SM m_currentState;
+    private EnemyStates_SM m_previousState;
+
+    [HideInInspector]
+    // TODO: Create all states
+
+    
+
+    private void Awake()
+    {
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        m_currentState.UpdateState();
+
+        if (m_previousState != null && m_previousState != m_currentState)
+        {
+            Debug.Log("GreenBlob state changed! " + m_previousState + " -> " + m_currentState);
+        }
+
+        m_previousState = m_currentState;
+	}
+}
